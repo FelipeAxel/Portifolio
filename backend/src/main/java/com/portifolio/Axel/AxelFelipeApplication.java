7bb82754523b016986146092bf271733cac13989
FelipeAxel <axelnovais96@gmail.com>
2022-09-19 16:16:41 -0300

    Inicialização do projeto portifolio

---
+++ backend/src/main/java/com/portifolio/Axel/AxelFelipeApplication.java
@@ -0,0 +1,13 @@
+package com.portifolio.Axel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxelFelipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxelFelipeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/portifolio/Axel/AxelFelipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portifolio.Axel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AxelFelipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
